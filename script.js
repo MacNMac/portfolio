cd17800016e4d1096847451504dfaedef2c9ffed
Alifian <133396252+MacNMac@users.noreply.github.com>
2025-02-05 07:19:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -151,18 +151,3 @@ const form = document.getElementById('contact-form');
                  }
              });
          });
-
-// Hamburger Menu Toggle
-const hamburger = document.getElementById('hamburger');
-const navLinks = document.getElementById('nav-links');
-
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-});
-
-// Close the menu when a link is clicked (optional)
-navLinks.addEventListener('click', () => {
-    if (navLinks.classList.contains('active')) {
-        navLinks.classList.remove('active');
-    }
-});
